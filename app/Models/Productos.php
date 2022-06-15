0a8849c4eb5b5b3a48bb8c2f83c0ea14f52521e0
victoriacusme <victoriacusme2001@gmail.com>
2022-06-15 00:12:11 -0500

    save

---
+++ app/Models/Productos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Productos extends Model
+{
+    use HasFactory;
+    protected $table='_producto';
+    public $timestamps=false;
+    public $fillable = ['producto','descripcion'];
+}
